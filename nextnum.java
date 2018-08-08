98f8738676fbe886de72d915887e48577c401eb5
sunnyhitha <41725679+sunnyhitha@users.noreply.github.com>
2018-08-08 13:43:14 +0530

    Create nextnum.java

---
+++ nextnum.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int s;
+		Scanner sc=new Scanner(System.in);
+		s=sc.nextInt();
+		s=s+1;
+		System.out.print(s);// your code goes here
+	}
+}
